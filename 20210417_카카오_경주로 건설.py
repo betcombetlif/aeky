f7f7deeb4efc8248a856bf4e6223067fc74bd8d4
yaekyoon.song <yaekyoon.song@kt.com>
2021-04-18 03:57:20 +0900

    2021-04-18

--- 20210417_카카오_경주로 건설.py
+++ 20210417_카카오_경주로 건설.py
@@ -1,1 +1,9 @@
 # https://programmers.co.kr/learn/courses/30/lessons/67259
+
+def solution(board):
+    answer = 0
+    return answer
+
+
+board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+print(solution(board))
--- 20210417_프로그래머스_점프와 순간이동.py
+++ 20210417_프로그래머스_점프와 순간이동.py
@@ -1,2 +1,18 @@
 # https://programmers.co.kr/learn/courses/30/lessons/12980
+# 통과
 
+def solution(n):
+    ans = 0
+
+    while n / 2 > 0:
+        if n % 2 == 1:
+            ans += 1
+            n -= 1
+        else:
+            n = n / 2
+
+    return ans
+
+
+n = 5
+print(solution(n))
